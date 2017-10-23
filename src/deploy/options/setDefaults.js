c9b1854a711b86be7a3442e9129242ad22de12af
Evan Siegel <evan@grailed.com>
2017-10-23 13:13:40 -0400

    Deploy.Options.setDefaults Marketplace Array
    - Sets marketplace to an array instead of a string in
    Deploy.Options.setDefaults.

--- src/deploy/options/setDefaults.js
+++ src/deploy/options/setDefaults.js
@@ -1,15 +1,35 @@
 const { execSync } = require("child_process")
-const { trim, toString } = require("ramda")
+const { trim, toString, compose, propOr } = require("ramda")
+
+const ENVIRONMENTS = ["staging", "production"]
+
+const MARKETPLACES = ["grailed", "heroine"]
+
+const ALL = "all"
+
+// setEnvironment :: Deploy.Options -> String
+const setEnvironment = propOr(ENVIRONMENTS[0], "environment")
+
+// setMarketplace :: Deploy.Options -> String
+const setMarketplace = ({ marketplace=MARKETPLACES[0] }) =>
+  (marketplace === ALL) ? MARKETPLACES : [marketplace]
+
+const DEFAULT_REF_GIT_COMMAND = "git rev-parse --abbrev-ref HEAD"
+// getDefaultReference :: String -> String
+const getDefaultReference = (cmd=DEFAULT_REF_GIT_COMMAND) => compose(
+  trim,
+  toString,
+  execSync
+)(cmd)
+
+// setReference :: Deploy.Options -> String
+const setReference = propOr(getDefaultReference(), "reference")
 
 // Deploy.Options.setDefaults :: Deploy.Options -> Promise<Deploy.Options>
-const setDefaults = ({
-  environment="staging",
-  marketplace="grailed",
-  reference=trim(toString(execSync("git rev-parse --abbrev-ref HEAD"))),
-}) => Promise.resolve({
-  environment,
-  marketplace,
-  reference,
+const setDefaults = (options) => Promise.resolve({
+  environment: setEnvironment(options),
+  marketplace: setMarketplace(options),
+  reference: setReference(options),
 })
 
 module.exports = setDefaults
