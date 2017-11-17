de081b5d5050db2a88a4aeb15916534a2fecf886
Evan Siegel <evan@grailed.com>
2017-11-16 22:13:06 -0500

    Color Coded Process Labels

--- src/git/command/execute.js
+++ src/git/command/execute.js
@@ -1,9 +1,17 @@
 import { trim, map, split } from "ramda"
 import { spawn } from "child_process"
-import { magenta } from "chalk"
+import chalk from "chalk"
 import * as Logger from "../../logger"
 import GitError from "../git-error"
 import refspec from "./refspec"
+
+const random = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+const randomColor = () => chalk.rgb(random(0, 255), random(0, 255), random(0, 255))
 
 // onChildClose :: Function, Function, Array<String>, Array<String> -> Number -> Git.Result
 const onChildClose = (resolve, reject, stdout, stderr) => (code) => {
@@ -13,9 +21,9 @@ const onChildClose = (resolve, reject, stdout, stderr) => (code) => {
 }
 
 // handleOutputData :: Array<String>, Logger -> String -> Void
-const handleOutputData = (repo, stdio, _Logger=Logger) => (data) => {
+const handleOutputData = (repo, stdio, color, _Logger=Logger) => (data) => {
   const lines = split("\n")(trim(data))
-  map((line) => _Logger.log(`${magenta.dim(repo)} ${line}`), lines)
+  map((line) => _Logger.log(`${color(repo)} ${line}`), lines)
   stdio.push(data)
 }
 
@@ -34,15 +42,17 @@ const execute = (command, _Logger=Logger, _spawn=spawn) =>
     let stdout = []
     let stderr = []
 
+    const color = randomColor()
+
     child
       .stdout
       .setEncoding("utf8")
-      .on("data", handleOutputData(command.repository, stdout, _Logger))
+      .on("data", handleOutputData(command.repository, stdout, color, _Logger))
 
     child
       .stderr
       .setEncoding("utf8")
-      .on("data", handleOutputData(command.repository, stderr, _Logger))
+      .on("data", handleOutputData(command.repository, stderr, color, _Logger))
 
     child
       .on("error", reject)
