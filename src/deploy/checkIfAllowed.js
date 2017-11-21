c575b5c4e279f6e1c699aeea277492acf23cb808
Evan Siegel <evan@grailed.com>
2017-11-21 17:13:21 -0500

    Log Dog if Allowed
    - Save logSuccess for when things finish.

--- src/deploy/checkIfAllowed.js
+++ src/deploy/checkIfAllowed.js
@@ -1,18 +1,18 @@
 import { Future } from "ramda-fantasy"
 import { compose, chain, map } from "ramda"
 import setBuildState from "./setBuildState"
-import { logSuccess } from "../logger"
+import { logDog } from "../logger"
 import * as Options from "./options"
 
-// logSuccessMessage :: Deploy.Options -> Deploy.Options
-const logSuccessMessage = (options) => {
-  logSuccess("Great! I'll get right on it.")
+// logAllowedMessage :: Deploy.Options -> Deploy.Options
+const logAllowedMessage = (options) => {
+  logDog("Great! I'll get right on it.")
   return options
 }
 
 // Deploy.checkIfAllowed :: Deploy.Options -> Future Error Deploy.Options
 const checkIfAllowed = compose(
-  map(logSuccessMessage),
+  map(logAllowedMessage),
   chain(Options.confirm),
   chain(setBuildState),
   Future.of
